35c056df4fc289825c4c85a9259a587633d1ac19
varun-mishra-226 <mishra.varun255@gmail.com>
2021-07-17 20:32:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dyte Task - Code Editor</h1>
+      <div>
+        <h2>File Explorer</h2>
+        <textarea name="" id="" cols="50" rows="10"></textarea>
+      </div>
+      <div>
+        <h2>Code Editor</h2>
+        <textarea name="" id="" cols="50" rows="10"></textarea>
+      </div>
+      <div>
+        <h2>Live View</h2>
+        <textarea name="" id="" cols="50" rows="10"></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default App;
